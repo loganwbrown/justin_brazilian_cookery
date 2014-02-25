ff3b837f479d9dbe75dd271a39946a96ba60d650
DPL-4 <DPL-4@DPL-4s-iMac.local>
2014-02-25 12:35:46 -0800

    Add Recipe Controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,11 +1,46 @@
 class ChefsController < ApplicationController
 
-  
+  def index
+    @chefs = Chef.all
+  end
 
   def new
     @chef = Chef.new
   end
 
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "New chef has been created."
+      redirect_to chef_path(@chef.id)
+    else
+      flash[:error] = "There was an error saving your form."
+      render :new
+    end
 
+    def show
+      @chef = Chef.find(params[:id])
+    end
 
+    def edit
+      @chef = Chef.find(params[:id])
+    end
+
+    def update
+      @chef = Chef.find(params[:id])
+      if @chef.update_attributes(chef_params)
+        flash[:notice] = "#{@chef.name} has been updated."
+        redirect_to chef_path(@chef.id)
+      else
+        flash[:error] = "There was a problem updating your form."
+        render :edit
+      end
+    end
+  end
 end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name, :show, :specialty, :avatar)
+  end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,53 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        flash[:success] = "New recipe has been created."
+        redirect_to recipe_path(@recipe.id)
+      else
+        flash[:error] = "There was an error saving your form."
+        render :new
+      end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update_attributes(recipe_params)
+        flash[:notice] = "#{@recipe.name} has been updated."
+        redirect_to recipe_path(@recipe.id)
+      else
+        flash[:error] = "There was a problem updating your form."
+        render :edit
+      end
+  end
+
+  def destroy
+
+  end
+
+
 end
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :cook_time, :instructions, :picture, :ingredients)
+  end
+
